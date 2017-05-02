bb95cf915bffbf0f04141fea89fde43bc60a647d
Yonat Sharon <yonat@ootips.org>
2017-05-02 10:19:54 +0300

    bumped version

---
+++ Example/Pods/Target Support Files/MiniLayout/MiniLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MiniLayout : NSObject
+@end
+@implementation PodsDummy_MiniLayout
+@end
---
+++ Example/Pods/Target Support Files/MultiSlider/MultiSlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MultiSlider : NSObject
+@end
+@implementation PodsDummy_MultiSlider
+@end
