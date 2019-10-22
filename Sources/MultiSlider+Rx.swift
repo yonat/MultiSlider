c29d9bf4428134fa893d4cfa215916906a039b14
Yuriy Makarov <yuri.makarow@yandex.ru>
2019-10-22 21:56:15 +0300

    Change

--- Sources/MultiSlider+Rx.swift
+++ Sources/MultiSlider+Rx.swift
@@ -1,11 +1,8 @@
-//
-//  File.swift
-//  
-//
 //  Created by Yuriy Makarov on 22.10.2019.
 //  yuri.makarow@yandex.ru
 
 import Foundation
+import RxSwift
 
 extension Reactive where Base: MultiSlider {
     
