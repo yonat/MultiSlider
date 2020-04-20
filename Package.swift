a31f987c330dfb949f731427e90f6ffe5662db12
Max Cantor <i@cantor.mx>
2020-04-20 00:28:54 -0700

    Fix package.swift iOS version for swiftui (#48)
    Co-authored-by: Max Cantor tbmb13-desktop <max@cantor.mx>

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MultiSlider",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v13),
     ],
     products: [
         .library(name: "MultiSlider", targets: ["MultiSlider"]),
